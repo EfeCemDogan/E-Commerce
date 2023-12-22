6947d5866f47e1328672a976f3ee2059185f3382
Efe Cem Dogan <125884274+EfeCemDogan@users.noreply.github.com>
2023-12-22 17:59:44 +0300

    Front-end

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,11 +1,12 @@
 import React, { createContext, useState } from "react";
 import all_product from "../Components/Assets/all_product";
+
   
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
     let cart = {};
-    for (let index = 0; index < all_product.length+; index++) {
+    for (let index = 0; index < all_product.length+1; index++) {
         cart[index] = 0;
     }
     return cart
@@ -13,11 +14,42 @@ const getDefaultCart = () => {
 
 const ShopContextProvider = (props) => {
 
-    const [carItems, setCartItems] = useState(getDefaultCart());
-    const constextValue = {all_product,carItems};
+    const [cartItems, setCartItems] = useState(getDefaultCart());
 
-    const addToCart = ()
+    const addToCart = (itemId) => {
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        console.log(cartItems);
+    }
+    
+    const removeFromCart = (itemId) => {
+        setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+    }
 
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for(const item in cartItems) {
+            if(cartItems[item]>0) {
+                let itemInfo = all_product.find((product)=>product.id===Number(item))
+                totalAmount += itemInfo.new_price * cartItems[item];
+            }
+        }
+        return totalAmount;
+
+    }
+
+    const getTotalCartItems = () => {
+        let totalItem = 0;
+        for (const item in cartItems)
+        {
+            if (cartItems[item]>0)
+            {
+                totalItem += cartItems[item];
+            }
+        }
+        return totalItem;
+    }
+
+    const constextValue = {getTotalCartItems, getTotalCartAmount, all_product,cartItems,addToCart,removeFromCart};
     return (
         <ShopContext.Provider value={constextValue}>
             {props.children}
